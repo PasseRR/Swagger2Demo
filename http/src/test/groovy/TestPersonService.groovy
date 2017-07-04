9a454d5278e3ac19456890f050b85b7d89153d38
PasseRR <xie__hai@sina.com>
2017-07-04 19:39:18 +0800

    spock unit test

--- http/src/test/groovy/TestPersonService.groovy
+++ http/src/test/groovy/TestPersonService.groovy
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.web.WebAppConfiguration
 import spock.lang.Specification
+import spock.lang.Unroll
 
 @WebAppConfiguration
 @ContextConfiguration(classes = DemoApplication.class)
@@ -12,20 +13,45 @@ class TestPersonService extends Specification {
     @Autowired
     PersonService personService;
 
-    def addPerson() {
+    @Unroll
+    // 动态spec名
+    def "addPerson:(idCardNo->#idCardNo, sex->#sex, name->#name), expect:#result"() {
+        // 前置条件 同setup
         given:
-        PersonVo personVo = new PersonVo();
-        personVo.setId(id);
-        personVo.setIdCardNo(cardNo);
-        personVo.setSex(sex);
-        personVo.setName(name)
-
+        def personVo = new PersonVo(
+            idCardNo: idCardNo,
+            sex: sex,
+            name: name
+        );
+        // 预期
         expect:
         result == this.personService.addPerson(personVo)
-
+        // 条件
         where:
-        id      |       cardNo      |       name        |       sex         ||   result
-        1       |       "5101"      |       "Jack"      |       "male"      ||   true
-        2       |       "123456"    |       "Lucy"      |       "female"    ||   true
+        // 数据定义方法一
+        // |用来分隔输入 ||用来分隔输出
+        idCardNo | name   | sex      || result
+        "5101"   | "Jack" | "male"   || true
+        "123456" | "Lucy" | "female" || true
+    }
+
+    @Unroll
+    def "modifyPerson:(id->#id, idCardNo->#idCardNo, sex->#sex, name->#name), expect:#result"(){
+        given:
+        def personVo = new PersonVo(
+            id: id,
+            idCardNo: idCardNo,
+            sex: sex,
+            name: name
+        );
+        expect:
+        result == this.personService.modifyPerson(personVo)
+        where:
+        // 数据定义方法二
+        id << [1, 2]
+        idCardNo << ["333", "444"]
+        name << ["zhangsan", "lisi"]
+        sex << ["male", "male"]
+        result << [true, true]
     }
 }
